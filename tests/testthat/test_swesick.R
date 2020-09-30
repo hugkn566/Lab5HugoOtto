0978c40f79312e6f70b2b9bd861acd400a465022
Otto Moen <ottmo242@student.liu.se>
2020-09-30 09:29:07 +0200

    More tests

--- tests/testthat/test_swesick.R
+++ tests/testthat/test_swesick.R
@@ -15,5 +15,30 @@ test_that("output is a dataframe", {
 test_that("Output is correct", {
   testvalue <- swesick(2005)
   
-  expect_equal(testvalue[c(1,2,3),6] , c("239929", "1210", "9156"))
+  expect_equal(nrow(testvalue), 96)
+  expect_equal(ncol(testvalue), 10)
+  
+  expect_equal(names(testvalue), c("kvartal", 
+                                   "ar",
+                                   "diagnoskapitel_text", 
+                                   "andel", 
+                                   "diagnoskapitel_kod",
+                                   "antal","antal_man",
+                                   "andel_kvinnor",
+                                   "antal_kvinnor",
+                                   "andel_man"))
+  
+  expect_equal(testvalue[c(1,2,3),6] , c("239929", 
+                                         "1210", 
+                                         "9156"))
+  
+  expect_equal(testvalue[c(1,2,3),3] , c("Samtliga", 
+                                         "Vissa infektionssjukdomar och parasitsjukdomar", 
+                                         "Tumörer"))
+})
+
+test_that("Encoding is UTF-8", {
+  testvalue <- swesick(2005)
+  
+  expect_equal(Encoding(frame$diagnoskapitel_text[3]), "UTF-8")
 })
